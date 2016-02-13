8df12d838e51c7fb9a1d1feb75ec952a38a587a2
Johanna Chisholm <johanna.chisholm@journalism.cuny.edu>
2016-02-13 15:25:54 -0500

    add my javascript file

---
+++ myjavascript.js
@@ -0,0 +1,17 @@
+var x = 4;
+var y = 3;
+var addition = x + y;
+document.getElementById("equation1").innerHTML = addition;
+var x = 4;
+var y = 3;
+var subtraction = x - y;
+document.getElementById("equation2").innerHTML = subtraction;
+var x = 4;
+var y = 3;
+var multiplication = x * y;
+document.getElementById("equation3").innerHTML = multiplication;
+var x = 4;
+var y = 3;
+var division = x / y;
+document.getElementById("equation4").innerHTML = division;
+
